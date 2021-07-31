222d3ea87705f4690464a23d6fedeb72269e05e0
Brambuca <localyridze@gmail.com>
2021-07-31 15:24:36 +0300

    Add close btn

--- pages/js/login-registration.js
+++ pages/js/login-registration.js
@@ -1,11 +1,15 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const closeButton = document.getElementById('close')
 
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
+    closeButton.classList.add("left")
 });
 
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
+    closeButton.classList.remove("left")
+
 });
